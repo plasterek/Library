431bffb645b892f07da2f2b6628023130906eddb
Patryk <patryk.dzieko@gmail.com>
2023-03-08 09:27:19 +0100

    Create user.ts
    Adding user class to this branch... fuck up :D

---
+++ src/user/user.ts
@@ -0,0 +1,11 @@
+import { v4 as newUuid } from "uuid";
+
+export class User {
+  readonly id: string;
+  constructor() {
+    this.id = newUuid();
+  }
+  getID(): string {
+    return this.id;
+  }
+}
